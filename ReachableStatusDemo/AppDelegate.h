1fdab92f0070375cc0a85dbd4b003d2a7d269142
lyjl1025822032 <m18272733524@163.com>
2017-11-03 09:47:07 +0800

    Initial Commit

---
+++ ReachableStatusDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReachableStatusDemo
+//
+//  Created by yao on 2017/11/3.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReachableStatusDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReachableStatusDemo
+//
+//  Created by yao on 2017/11/3.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
